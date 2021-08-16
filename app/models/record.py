b48b9914eb9ead7e73a66143d90d26073105d629
Ji <jikyung@gmail.com>
2021-08-16 13:04:11 -0400

    add record, course models, & update user, __init__

---
+++ app/models/record.py
@@ -0,0 +1,21 @@
+from .db import db
+
+
+class Record(db.Model):
+    __tablename__ = 'records'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey(
+        'courses.id'), nullable=False)
+    time = db.Column(db.Integer, nullable=False)
+    character = db.Column(db.String, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'course_id': self.course_id,
+            'time': self.time,
+            'character': self.character,
+        }
--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,9 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    character_pfp = db.Enum(db.String, nullable=False)
+    bio = db.Text(db.String, nullable=False)
+    country_code = db.Integer(db.Integer)
 
     @property
     def password(self):
@@ -26,5 +29,8 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'character_pfp': = self.character_pfp,
+            'bio': = self.bio,
+            'country_code': self.country_code
         }
