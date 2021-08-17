8e0b645f372a55dba86ab7ed2dde38bbb9fece54
meagan <meagan.h.smith@gmail.com>
2021-08-17 15:57:24 -0500

    Start grid for nav bar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,35 +1,54 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import LogoutButton from './auth/LogoutButton';
 import './NavBar.css'
 
 const NavBar = () => {
+  const sessionUser = useSelector(state => state.session.user)
   return (
-    <nav>
+    <nav className="nav-bar-container">
       <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
+        <div className="home-link">
+          <li>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+        </div>
+
+        <div className="login-link">
+          <li>
+            <NavLink to='/login' exact={true} activeClassName='active'>
+              Login
+            </NavLink>
+          </li>
+        </div>
+
+        <div className="sign-up-link">
+          <li>
+            <NavLink to='/sign-up' exact={true} activeClassName='active'>
+              Sign Up
+            </NavLink>
+          </li>
+        </div>
+
+        <div className="users-link">
+          <li>
+            <NavLink to='/users' exact={true} activeClassName='active'>
+              Users
+            </NavLink>
+          </li>
+        </div>
+
+        <div className="logout-button-link">
+          <li>
+            <LogoutButton user={ sessionUser } />
+              {/* {(sessionUser)? displaylogout: <Redirect to='/' />}
+            </LogoutButton> */}
+          </li>
+        </div>
+        
       </ul>
     </nav>
   );
