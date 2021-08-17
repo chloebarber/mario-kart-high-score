4ae90c1f743ccaf3ba683f1bd21d1d526c854825
Antonio Davila-Olivares <ajd57@nau.edu>
2021-08-17 09:20:38 -0700

    removed game import in index.js

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
-import game from './store'
+
 
 
 const store = configureStore();
