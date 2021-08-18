04e473106586cd16d137fcf39333fd3e8fbc5f96
meagan <meagan.h.smith@gmail.com>
2021-08-18 16:37:27 -0500

    Add record form and thunk

--- react-app/src/components/addRecordForm/index.js
+++ react-app/src/components/addRecordForm/index.js
@@ -3,41 +3,52 @@ import { useState } from 'react';
 import { createRecord } from '../../store/record'
 // import './AddReviewForm.css';
 
-const AddRecordForm = ({ user, listing }) => {
+const AddRecordForm = () => {
     const sessionUser = useSelector(state => state.session.user)
+    const courseInfo = useSelector((state) => state.courseInfo)
+
     const dispatch = useDispatch();
+
     const [user_id, setUser_id] = useState(sessionUser.id);
-    const [course_id, setCourse_id] = useState();
+    const [course_id, setCourse_id] = useState(courseInfo.id);
     const [time, setTime] = useState();
     const [character, setCharacter] = useState()
     // console.log(+listing)
+
+    // const createUserId = (e) => setUser_id(e.target.value);
+    // const createCourse_Id = (e) => setCourse_id(e.target.value);
+    const createTime = (e) => setTime(e.target.value);
+    const createCharacter = (e) => setCharacter(e.target.value);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         const addRecord = {
-            review,
-            userId: user.id,
-            spotId: +listing
+           user_id,
+           course_id,
+           time,
+           character
         };
+
         await dispatch(createRecord(addRecord))
-        setReview('');
+
     };
 
     return (
-        <div className='form__container2'>
-            <form className='review__form2' onSubmit={handleSubmit}>
-                <textarea
-                    className='review-text-area'
-                    type='text'
-                    placeholder='Tell us about your stay...'
-                    value={review}
-                    onChange={(e) => setReview(e.target.value)}
-                />
-                <button className='reviewSubmit__button' type='submit'>Submit</button>
+        <div className='record-div'>
+            <form className='record-form' onSubmit={handleSubmit}>
+                <label className="time">Enter your time:
+                    <input type="integer" onChange={createTime}/>
+                </label>
+
+                <label className="character">Enter your character:
+                    <input type="text" onChange={createCharacter}/>
+                </label>
+
+                <button className='recordSubmit__button' type='submit'>Submit</button>
             </form>
         </div>
     )
 }
 
-export default AddReviewForm;
+export default AddRecordForm;
