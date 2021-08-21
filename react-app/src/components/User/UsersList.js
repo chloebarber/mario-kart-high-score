e5b636cf40063f9ead4d3a9fd1cfbe55123027ca
meagan <meagan.h.smith@gmail.com>
2021-08-21 11:26:47 -0500

    Add floating image, update font, add background color for mkracers page

--- react-app/src/components/User/UsersList.js
+++ react-app/src/components/User/UsersList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import './UsersList.css'
 
 function UsersList() {
   const [users, setUsers] = useState([]);
@@ -16,16 +17,22 @@ function UsersList() {
   const userComponents = users.map((user) => {
     return (
       <li key={user.id}>
-        <NavLink to={`/users/${user.id}`}>{user.username}</NavLink>
+        <NavLink className="user-link" to={`/users/${user.id}`}>{user.username}</NavLink>
       </li>
     );
   });
 
   return (
-    <>
-      <h1>User List: </h1>
+    <div className="user-list-div">
+      <div className="start-img-div">
+        <img src="https://www.pinclipart.com/picdir/big/201-2015536_mario-kart-8-deluxe-mario-kart-lakitu-png.png" alt="lakitu-start" className="start-img"/>
+      </div>
+
+      <div className="mk-racers-text-div">
+        <h1 className="mk-racers-text">Mario Kart Racers!: </h1>
       <ul>{userComponents}</ul>
-    </>
+      </div>
+    </div>
   );
 }
 
